63b575bf91137c431ac5ca5690ce82ba46d81b13
eseoseodion <ehodion1@sheffield.ac.uk>
2023-10-19 15:06:57 +0100

    Rendered the app titles into columns

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2) #st is the module, columns is the method
-                           #this method will return two column objects
+                           #this method will return two column objects & store them in the two variables
 
 with col1:
     st.image("images/photo.png")
@@ -24,3 +25,15 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";") #sep can be omitted if the data in the files are separated by ","
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
