cf1188e766b4e5f04307e8324ad1454ed66a08a8
eseoseodion <ehodion1@sheffield.ac.uk>
2023-10-19 23:34:26 +0100

    Sent emails using the form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    message = st.text_area() #this allows us to enter multiple lines of text as opposed to a single line
-    button = st.form_submit_button() #special button design to submit its assigned form
+    raw_message = st.text_area("Ypur message") #this allows us to enter multiple lines of text as opposed to a single line
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}"""
+    
+    button = st.form_submit_button("Submit") #special button design to submit its assigned form
     print(button)
     if button:
-        print("Pressed")
+       send_email(message)
+       st.info("Your email was sent successfully.")
--- send_email.py
+++ send_email.py
@@ -1,22 +1,17 @@
 #SEHD EMAILS
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "odioneseose@gmail.com"
-password = "lnvt mjmo wgib xaag"
+    username = "odioneseose@gmail.com"
+    password = "lnvt mjmo wgib xaag"
 #lnvt mjmo wgib xaag
 
-receiver = "odioneseose@gmail.com"
-context = ssl.create_default_context()
+    receiver = "odioneseose@gmail.com"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-""" #when sending a subject, add a break line
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
